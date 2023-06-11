baa6744954e33bf61c2471fb34c06d30b255e4b7
Jean Bacquaert <Jean.bacquaert@gmail.com>
2023-06-11 17:48:20 +0200

    examenHulp

---
+++ ExamenHulp/JPA/2.filmjson-jpa/src/main/java/herexamen2/herexamen2/Herexamen2Application.java
@@ -0,0 +1,13 @@
+package herexamen2.herexamen2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Herexamen2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Herexamen2Application.class, args);
+	}
+
+}
---
+++ ExamenHulp/JPA/2.filmjson-jpa/src/main/java/herexamen2/herexamen2/jpa/Movie.java
@@ -0,0 +1,41 @@
+package herexamen2.herexamen2.jpa;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+public class Movie{
+    public String Title;
+    public String Year;
+    public String Rated;
+    public String Released;
+    public String Runtime;
+    public String Genre;
+    public String Director;
+    public String Writer;
+    public String Actors;
+    public String Plot;
+    public String Language;
+    public String Country;
+    public String Awards;
+    public String Poster;
+    public String Metascore;
+    public String imdbRating;
+    public String imdbVotes;
+    public String imdbID;
+    public String Type;
+    public String Response;
+    public List<String> Images;
+
+    //return actors
+
+    public String getActors() {
+        return Actors;
+    }
+}
